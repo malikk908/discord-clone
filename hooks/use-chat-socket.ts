6920195db98be35e9ac1b0e79169ee45cac10a92
malikk908 <136706872+malikk908@users.noreply.github.com>
2023-10-30 00:18:53 +0500

    updateMessage socket

--- hooks/use-chat-socket.ts
+++ hooks/use-chat-socket.ts
@@ -32,6 +32,34 @@ export const useChatSocket = ({
             return
         }
 
+        socket.on(updateKey, (message: MessageWithMemberWithProfile) => {
+            queryClient.setQueryData([queryKey], (oldData: any) => {
+
+                if(!oldData || !oldData.pages || oldData.pages.length === 0) {
+                    return oldData
+                }
+
+                const newData = oldData.pages.map((page: any) => {
+                    return {
+                        ...page,
+                        items: page.items.map((item: MessageWithMemberWithProfile) => {
+                            if(item.id === message.id){
+                                return message;
+                            }
+
+                            return item;
+                        })
+                    }
+                });
+
+                return {
+                    ...oldData,
+                    pages: newData
+                }
+                
+            })
+        })
+
         socket.on(addKey, (message: MessageWithMemberWithProfile) => {
             queryClient.setQueryData([queryKey], (oldData: any) => {
                 if(!oldData || !oldData.pages || oldData.pages.length === 0) {
